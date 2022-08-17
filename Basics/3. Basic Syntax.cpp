c2a40262f3e8e2548d12a7266de078499464f896
George Smith <40671362+GLSmith155@users.noreply.github.com>
2022-08-17 10:56:54 -0400

    Update 3. Basic Syntax.cpp

--- Basics/3. Basic Syntax.cpp
+++ Basics/3. Basic Syntax.cpp
@@ -19,3 +19,36 @@ int main()
 
     return 0;
 }
+// The array class.
+// The Array Class is very similar to built-in arrays, 
+// but is object-oriented.
+
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+    // If some Ints are defined, then the undefined one's will be 0.
+    array<int, 5> myIntegerArray{1}; // Output:  1,0,0,0,0
+    
+    for (int a : myIntegerArray) {
+        cout << a << endl;
+    }
+    
+    // Use the array class to find the size of the array:
+    cout << "Size of array: " << myIntegerArray.size() << endl;
+
+
+// TwiceNumber Challenge. Use the array class to create an array 
+// of size 10 with only multiples of 2.
+    array<int, 10> myNums;
+    for (int i = 0; i < myNums.size(); i++) {
+        myNums[i] = i * 2;
+    }
+    for (int element : myNums) {
+        cout << element << endl;
+    }
+    return 0;
+}
+
