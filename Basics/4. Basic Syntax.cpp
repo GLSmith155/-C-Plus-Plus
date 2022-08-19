5a010ce73b3351279ea6c2871c468786975a4b9b
George Smith <40671362+GLSmith155@users.noreply.github.com>
2022-08-19 11:04:50 -0400

    Update 4. Basic Syntax.cpp
    Vectors

--- Basics/4. Basic Syntax.cpp
+++ Basics/4. Basic Syntax.cpp
@@ -9,13 +9,52 @@
 #include <string>
 using namespace std;
 
+/** Vector Methods / Member functions:
+ * push_back - Used to extend vector with a new element at end.
+ * pop_back - Used to remove element at end of vector.
+ * front - returns front element.   back - returns last element.
+ * instert - can insert an element anywhere in vector.
+ * begin - returns the beginning of the vector. Used to insert.
+**/
 int main()
 {
+// someVec is an example of a vector created without 1st specifying
+// size. anotherVec is a vector example with a specific size.
     vector<int> someVec;
     vector <string> anotherVec(3);
 
     someVec.push_back(1);
     someVec.push_back(2);
     someVec.push_back(3);
+
+    anotherVec[0] = "Keith";
+    anotherVec[1] = "Jeremy";
+    anotherVec[2] = "Shirt";
+    // Below we add Salsa to anotherVec using push_back.
+    anotherVec.push_back("Salsa");
+    
+    cout << "The size of someVec is: " << someVec.size() << endl;
+    
+    for (int val : someVec) {
+        cout << val << endl;
+    }
+    
+    cout << endl;
+    
+    for (string name : anotherVec) {
+        cout << name << endl;
+    }
+    cout << "The size of anotherVec is: " << anotherVec.size() << endl;
+    
+    cout << "Front and back: " << endl;
+    cout << "front: " << anotherVec.front() << endl;
+    cout << "back: " << anotherVec.back() << endl;
+    
+    anotherVec.pop_back();
+// Where to begin insertion is first, followed by what we insert.
+    anotherVec.insert(anotherVec.begin(), "Don");
+
+    cout << "now, front is " << anotherVec.front() << endl;
+    cout << "now, back is " << anotherVec.back() << endl;
     return 0;
 }
